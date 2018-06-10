b387c701d86af96bf8d3d91df8e152ed2e37f530
Hp Pc <Hp Pc@192.168.0.18>
2018-06-10 23:11:32 +0530

    dev1 - changes program1

--- SunGITProj/src/com/nt/test/SampleApp.java
+++ SunGITProj/src/com/nt/test/SampleApp.java
@@ -1,13 +1,17 @@
 package com.nt.test;
 
 public class SampleApp {
-
+	public static int sum(int x,int y){
+		return x+y;
+		}
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 		System.out.println("hello");
 		  for(int i=1;i<=10;++i){
 			System.out.println(i);
 		}
+		  System.out.println("add=="+SampleApp.sum(100,200));
      //hi
 		//hello
 	}
